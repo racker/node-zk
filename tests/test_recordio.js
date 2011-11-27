83f1e3c885e914857193ec0dbf1012b42cb60458
Paul Querna <pquerna@apache.org>
2011-11-26 18:59:50 -0800

    add recordio tagging

--- tests/test_recordio.js
+++ tests/test_recordio.js
@@ -26,8 +26,8 @@ exports['test_simple'] = function(test, assert) {
       newBuf;
 
   arc1 = new Archive();
-  arc1.serialize_int(42);
-  arc1.serialize_buffer(origBuffer);
+  arc1.serialize_int("leading int", 42);
+  arc1.serialize_buffer("myBuffer", origBuffer);
 
   arc1buf = arc1.toBuffer();
   assert.isNotNull(newBuf);
@@ -35,8 +35,8 @@ exports['test_simple'] = function(test, assert) {
   arc2 = new Archive();
 
   arc2.fromBuffer(arc1buf);
-  assert.equal(42, arc2.deserialize_int());
-  newBuf = arc2.deserialize_buffer();
+  assert.equal(42, arc2.deserialize_int("leading int"));
+  newBuf = arc2.deserialize_buffer("myBuffer");
   assert.isNotNull(newBuf);
   assert.equal(3, newBuf.length);
 
@@ -50,8 +50,8 @@ exports['test_strings'] = function(test, assert) {
       newBuf;
 
   arc1 = new Archive();
-  arc1.serialize_string("hello world");
-  arc1.serialize_string("end universe");
+  arc1.serialize_string("str1", "hello world");
+  arc1.serialize_string("str2", "end universe");
 
   arc1buf = arc1.toBuffer();
   assert.isNotNull(arc1buf);
@@ -59,8 +59,8 @@ exports['test_strings'] = function(test, assert) {
   arc2 = new Archive();
   arc2.fromBuffer(arc1buf);
 
-  assert.equal("hello world", arc2.deserialize_string());
-  assert.equal("end universe", arc2.deserialize_string());
+  assert.equal("hello world", arc2.deserialize_string("str1"));
+  assert.equal("end universe", arc2.deserialize_string("str2"));
 
   test.finish();
 };
@@ -72,8 +72,8 @@ exports['test_longs'] = function(test, assert) {
       bignum = new Int64("1152921504606846976");
 
   arc1 = new Archive();
-  arc1.serialize_long(52);
-  arc1.serialize_long(bignum);
+  arc1.serialize_long("long1", 52);
+  arc1.serialize_long("long2", bignum);
 
   arc1buf = arc1.toBuffer();
   assert.isNotNull(arc1buf);
@@ -81,8 +81,8 @@ exports['test_longs'] = function(test, assert) {
   arc2 = new Archive();
   arc2.fromBuffer(arc1buf);
 
-  assert.equal(52, arc2.deserialize_long());
-  assert.equal("2921504606846976", arc2.deserialize_long().toOctetString());
+  assert.equal(52, arc2.deserialize_long("long1"));
+  assert.equal("2921504606846976", arc2.deserialize_long("long2").toOctetString());
 
   test.finish();
 };
@@ -95,8 +95,8 @@ exports['test_buffers'] = function(test, assert) {
       newBuf;
 
   arc1 = new Archive();
-  arc1.serialize_buffer(new Buffer("hello world"));
-  arc1.serialize_buffer(new Buffer("end universe"));
+  arc1.serialize_buffer("buf1", new Buffer("hello world"));
+  arc1.serialize_buffer("buf2", new Buffer("end universe"));
 
   arc1buf = arc1.toBuffer();
   assert.isNotNull(arc1buf);
@@ -104,8 +104,8 @@ exports['test_buffers'] = function(test, assert) {
   arc2 = new Archive();
   arc2.fromBuffer(arc1buf);
 
-  assert.equal("hello world", arc2.deserialize_buffer().toString());
-  assert.equal("end universe", arc2.deserialize_buffer().toString());
+  assert.equal("hello world", arc2.deserialize_buffer("buf1").toString());
+  assert.equal("end universe", arc2.deserialize_buffer("buf2").toString());
 
   test.finish();
 };
@@ -119,27 +119,27 @@ exports['test_vectors'] = function(test, assert) {
       i, j;
 
   arc1 = new Archive();
-  arc1.serialize_int(200);
-  arc1.serialize_start_vector(arr.length);
+  arc1.serialize_int("first int", 200);
+  arc1.serialize_start_vector("array start", arr.length);
   for (i = 0; i < arr.length; i++) {
-    arc1.serialize_string(arr[i]);
+    arc1.serialize_string(i, arr[i]);
   }
-  arc1.serialize_end_vector();
-  arc1.serialize_int(555);
+  arc1.serialize_end_vector("end vector");
+  arc1.serialize_int("trailing int", 555);
 
   arc1buf = arc1.toBuffer();
   assert.isNotNull(arc1buf);
 
   arc2 = new Archive();
   arc2.fromBuffer(arc1buf);
-  assert.equal(200, arc2.deserialize_int());
-  i = arc2.deserialize_start_vector();
+  assert.equal(200, arc2.deserialize_int("first int"));
+  i = arc2.deserialize_start_vector("array start");
   assert.equal(i, arr.length);
   for (j = 0; j < i; j++) {
-    assert.equal(arr[j], arc2.deserialize_string());
+    assert.equal(arr[j], arc2.deserialize_string(j));
   }
-  arc2.deserialize_end_vector();
-  assert.equal(555, arc2.deserialize_int());
+  arc2.deserialize_end_vector("end vector");
+  assert.equal(555, arc2.deserialize_int("trailing int"));
 
   test.finish();
 };
